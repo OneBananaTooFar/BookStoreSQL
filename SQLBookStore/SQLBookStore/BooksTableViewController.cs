3dc8331c466bd81a67acc1d41cb44c4b022d8698
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2018-01-15 13:15:29 -0700

    SQLBookStore

---
+++ SQLBookStore/SQLBookStore/BooksTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace SQLBookStore
+{
+    public partial class BooksTableViewController : UITableViewController
+    {
+        public BooksTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ SQLBookStore/SQLBookStore/Classes/DataBaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SQLBookStore.Classes
+{
+    public class DataBaseHelper
+    {
+        public DataBaseHelper()
+        {
+        }
+
+        public static bool Insert <T>(ref T data, string db_path)
+        {
+
+            using (var conn = new SQLite.SQLiteConnection(db_path))
+
+
+            return false;
+
+        }
+
+
+    }
+}
